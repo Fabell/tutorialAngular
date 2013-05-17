b3dd4ce175207200e0723b187753a04e9492b302
Fabian Ellenberger <fabian.ellenberger@centralway.com>
2013-05-17 17:58:05 +0200

    no message

--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -1,7 +1,21 @@
-'use strict';
-
-/* jasmine specs for controllers go here */
-
-describe('controllers', function() {
-
+describe('PhoneCat controllers', function() {
+ 
+  describe('PhoneListCtrl', function(){
+    var scope, ctrl;
+ 
+    beforeEach(function() {
+      scope = {},
+      ctrl = new PhoneListCtrl(scope);
+    });
+ 
+ 
+    it('should create "phones" model with 3 phones', function() {
+      expect(scope.phones.length).toBe(3);
+    });
+ 
+ 
+    it('should set the default value of orderProp model', function() {
+      expect(scope.orderProp).toBe('age');
+    });
+  });
 });
